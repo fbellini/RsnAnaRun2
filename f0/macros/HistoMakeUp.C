7c2148b6e36461ae5354fa3f149d52bbcc707250
Francesca Bellini <fbellini@cern.ch>
2017-08-31 18:45:48 +0200

    Added simple macros for cosmetics

---
+++ f0/macros/HistoMakeUp.C
@@ -0,0 +1,26 @@
+void HistoMakeUp(TH1 * histo, 
+		 Color_t color = kBlack,
+		 Int_t marker = 20,
+		 TString titleX = "", 
+		 TString titleY = "")
+{
+  if (!histo) return;
+  TAxis * xaxis = (TAxis*)histo->GetXaxis();
+  if (!titleX.IsNull()) xaxis->SetTitle(titleX.Data());
+  xaxis->SetTitleOffset(1.1);
+  xaxis->SetTitleSize(0.05);
+  xaxis->SetLabelSize(0.05);
+  TAxis * yaxis = (TAxis*)histo->GetYaxis();
+  if (!titleX.IsNull()) yaxis->SetTitle(titleY.Data());
+  yaxis->SetTitleOffset(1.2);
+  yaxis->SetTitleSize(0.05);
+  yaxis->SetLabelSize(0.05);
+  histo->SetLineColor(color);
+  histo->SetLineWidth(2);
+  histo->SetMarkerStyle(marker);
+  histo->SetMarkerColor(color);
+  histo->SetFillColor(color);
+  histo->SetFillStyle(0);
+  histo->SetMarkerSize(1.);
+  return;
+}
---
+++ f0/macros/SetStyle.C
@@ -0,0 +1,41 @@
+void SetStyle(Bool_t graypalette = 0) {
+  cout << "Setting style!" << endl;
+  
+  gStyle->Reset("Plain");
+  gStyle->SetOptTitle(0);
+  gStyle->SetOptStat(0);
+  if(graypalette) gStyle->SetPalette(8,0);
+  else gStyle->SetPalette(1);
+  gStyle->SetCanvasColor(10);
+  gStyle->SetCanvasBorderMode(0);
+  gStyle->SetFrameLineWidth(1);
+  gStyle->SetFrameFillColor(kWhite);
+  gStyle->SetPadColor(10);
+  gStyle->SetPadTickX(1);
+  gStyle->SetPadTickY(1);
+  gStyle->SetPadBottomMargin(0.15);
+  gStyle->SetPadLeftMargin(0.15);
+  gStyle->SetHistLineWidth(1);
+  gStyle->SetHistLineColor(kRed);
+  gStyle->SetFuncWidth(2);
+  gStyle->SetFuncColor(kGreen);
+  gStyle->SetLineWidth(2);
+  gStyle->SetLabelSize(0.045,"xyz");
+  gStyle->SetLabelOffset(0.01,"y");
+  gStyle->SetLabelOffset(0.01,"x");
+  gStyle->SetLabelColor(kBlack,"xyz");
+  gStyle->SetTitleSize(0.06,"xyz");
+  gStyle->SetTitleOffset(1.25,"y");
+  gStyle->SetTitleOffset(1.2,"x");
+  gStyle->SetTitleFillColor(kWhite);
+  gStyle->SetTextSizePixels(26);
+  gStyle->SetTextFont(42);
+  //  gStyle->SetTickLength(0.04,"X");  gStyle->SetTickLength(0.04,"Y"); 
+
+  gStyle->SetLegendBorderSize(0);
+  gStyle->SetLegendFillColor(kWhite);
+  //  gStyle->SetFillColor(kWhite);
+  gStyle->SetLegendFont(42);
+
+
+}
