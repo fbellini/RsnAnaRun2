69541ffacce4ffee3166c467f543d13ea8086e64
alessandralorenzo <30314102+alessandralorenzo@users.noreply.github.com>
2017-08-31 19:16:17 +0200

    optimization of rebin, projections and cosmetics

--- f0/macros/f0_bkg.C
+++ f0/macros/f0_bkg.C
@@ -12,42 +12,70 @@ void f0_bkg (){
 
 
   /* like-sign pairs combination */
-  TH3F * hLikePP = (TH3F*)list->FindObject("RsnTaskF0_f0_LikePP");
-  TH3F * hLikeMM = (TH3F*)list->FindObject("RsnTaskF0_f0_LikeMM");
-  TH3F * hUnlikePM = (TH3F*)list->FindObject("RsnTaskF0_f0_UnlikePM");
+  TH3F * hOrLikePP = (TH3F*)list->FindObject("RsnTaskF0_f0_LikePP");
+  TH3F * hOrLikeMM = (TH3F*)list->FindObject("RsnTaskF0_f0_LikeMM");
+  TH3F * hOrUnlikePM = (TH3F*)list->FindObject("RsnTaskF0_f0_UnlikePM");
+  TH3F * hLikePP = hOrLikePP->Clone("hLikePP");
+  TH3F * hLikeMM = hOrLikeMM->Clone("hLikeMM");
+  TH3F * hUnlikePM = hOrUnlikePM->Clone("hUnlikePM");
   hLikeMM->Add(hLikePP);
-  TH1 * hProjXYLikePPMM = hLikeMM->Project3D("xy");
-  hUnlikePM->Add(hLikeMM, -1);
-  TH1 * hProjXYUnlikePM = hUnlikePM->Project3D("xy");
+  TH3F * hSumLikeMMPP = hLikeMM->Clone("hSumLikeMMPP");
+  hUnlikePM->Add(hSumLikeMMPP, -1);
+  TH2D * hProjXYUnlikePM = hUnlikePM->Project3D("xy");
+  TH1 * hLastProjYLikePM = hProjXYUnlikePM->ProjectionY();
+  TH1 * hLastProjYRebLikePM  = hLastProjYLikePM->RebinX(5,"hLastProjYRebLikePM");
+  hLastProjYRebLikePM->SetTitle("LSB Subtracted");
+  hLastProjYRebLikePM->GetXaxis()->SetTitle("M_{inv}(GeV/#it{c^{2}})");
+  hLastProjYRebLikePM->GetYaxis()->SetTitle("pairs");
+  hLastProjYRebLikePM->SetMarkerStyle(kFullCircle);
 
 
   /* event mixing */
-  TH3F * hMixingPP = (TH3F*)list->FindObject("RsnTaskF0_f0_MixingPP");
-  TH3F * hMixingMM = (TH3F*)list->FindObject("RsnTaskF0_f0_MixingMM");
-  TH3F * hMixingPM = (TH3F*)list->FindObject("RsnTaskF0_f0_MixingPM");
+  TH3F * hOrMixingPP = (TH3F*)list->FindObject("RsnTaskF0_f0_MixingPP");
+  TH3F * hOrMixingMM = (TH3F*)list->FindObject("RsnTaskF0_f0_MixingMM");
+  TH3F * hOrMixingPM = (TH3F*)list->FindObject("RsnTaskF0_f0_MixingPM");
+  TH3F * hMixingPP = hOrMixingPP->Clone("hMixingPP");
+  TH3F * hMixingMM = hOrMixingMM->Clone("hMixingMM");
+  TH3F * hMixingPM = hOrMixingPM->Clone("hMixingPM");
   hMixingMM->Add(hMixingPP);
-  TH1 * hProjXYMixingPPMM = hMixingMM->Project3D("xy");
-  hMixingPM->Add(hMixingMM, -1);
-  TH1 * hProjXYMixingPM = hMixingPM->Project3D("xy");
+  TH3F * hSumMixingMMPP = hMixingMM->Clone("hSumMixingMMPP");
+  hMixingPM->Add(hSumMixingMMPP, -1);
+  TH2D * hProjXYMixingPM = hMixingPM->Project3D("xy");
+  TH1 * hLastProjYMixingPM = hProjXYMixingPM->ProjectionY();
+  TH1 * hLastProjYRebMixingPM  = hLastProjYMixingPM->RebinX(5,"hLastProjYRebMixingPM");
+  hLastProjYRebMixingPM->SetTitle("MEB Subtracted");
+  hLastProjYRebMixingPM->GetXaxis()->SetTitle("M_{inv}(GeV/#it{c^{2}})");
+  hLastProjYRebMixingPM->GetXaxis()->SetRangeUser(0.6,1.2);
+  hLastProjYRebMixingPM->GetYaxis()->SetTitle("pairs");
+  hLastProjYRebMixingPM->SetMarkerStyle(kOpenCircle);
 
 
   /* write output */
-  TCanvas * c1 = new TCanvas("c1","c1", 1200, 800);
-  c1->Divide(3,2);
-  c1->cd(1); hProjXYLikePPMM->Draw("colz");
-  //c1->cd(2); hLikePP->Draw("colz");
-  c1->cd(3); hProjXYUnlikePM->Draw("colz");
-  c1->cd(4); hProjXYMixingPPMM->Draw("colz");
-  //c1->cd(5); hMixingPP->Draw("colz");
-  c1->cd(6); hProjXYMixingPM->Draw("colz");
-  c1->Print("bgSubtraction.pdf");
+  TCanvas * c1 = new TCanvas("c1","LSB Subtracted", 1200, 800);
+  hLastProjYRebLikePM->Draw();
+  TCanvas * c2 = new TCanvas("c2","MEB Subtracted", 1200, 800);
+  hLastProjYRebMixingPM->Draw();
+  TCanvas * c3 = new TCanvas("c3","Compared (LSB and MEB)", 1200, 800);
+  hLastProjYRebLikePM->SetLineColor(kRed);
+  hLastProjYRebLikePM->SetLineWidth(2);
+  hLastProjYRebLikePM->SetMarkerColor(kRed);
+  hLastProjYRebLikePM->SetFillColor(kRed);
+  hLastProjYRebLikePM->Draw();
+  hLastProjYRebMixingPM->SetLineColor(kBlue);
+  hLastProjYRebMixingPM->SetLineWidth(2);
+  hLastProjYRebMixingPM->SetMarkerColor(kBlue);
+  hLastProjYRebMixingPM->SetFillColor(kBlue);
+  hLastProjYRebMixingPM->Draw("SAME");
+  gPad->BuildLegend();
+  
+  c1->Print("LSB.pdf");
+  c2->Print("MEB.pdf");
+  c3->Print("Compared (LSB and MEB).pdf");
 
   TFile *fout = TFile::Open("bgSubtraction.root", "RECREATE");
   fout->cd();
-  hProjXYLikePPMM->Write();
-  hProjXYUnlikePM->Write();
-  hProjXYMixingPPMM->Write();
-  hProjXYMixingPM->Write();
+  hLastProjYRebLikePM->Write();
+  hLastProjYRebMixingPM->Write();
   fout->Close();
 
 }
