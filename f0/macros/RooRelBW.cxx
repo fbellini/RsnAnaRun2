c6967d42341839c0f0e08ca9a57a59b3998b0414
alessandralorenzo <30314102+alessandralorenzo@users.noreply.github.com>
2017-12-13 18:27:08 +0100

    check on the evaluation -- f0 decays in pion+pion

--- f0/macros/RooRelBW.cxx
+++ f0/macros/RooRelBW.cxx
@@ -42,14 +42,14 @@ ClassImp(RooRelBW)
  Double_t RooRelBW::evaluate() const 
  { 
    //expression for function BW with gamma=f(width,m_pi,m_K)
-   const Double_t mkaon2 = 0.4937*0.4937; //GeV
+   //const Double_t mkaon2 = 0.4937*0.4937; //GeV
    const Double_t mpion2 = 0.1396*0.1396; //GeV
    Double_t mass2 = (mass*mass);
    Double_t mass4 = (mass2*mass2);
    Double_t factx4 = 1./(x*x*x*x);
-   Double_t diffnum = (x*x)-mpion2-mkaon2;
-   Double_t prod = 4*(mpion2*mkaon2);
-   Double_t diffden = (mass2-mpion2-mkaon2);   
+   Double_t diffnum = (x*x)-mpion2-mpion2;
+   Double_t prod = 4*(mpion2*mpion2);
+   Double_t diffden = (mass2-mpion2-mpion2);   
    Double_t ratio = (diffnum*diffnum-prod)/(diffden*diffden-prod);
    Double_t gamma = (width*mass4*factx4)*(TMath::Power(ratio, 1.5));
    Double_t arg = (x*x)-mass2;
